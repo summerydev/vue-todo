eaed7a75d1df317f7d63c37f28de9813c99c77a9
summer <summery.dev@gmail.com>
2022-10-24 16:57:38 +0900

    전체 삭제 기능 구현

---
+++ src/components/TodoFooter.vue
@@ -0,0 +1,20 @@
+<template>
+  <footer>
+    <button v-on:click="removeAll">전체 삭제</button>
+  </footer>
+</template>
+
+<script>
+export default {
+  methods: {
+    removeAll() {
+      if (confirm("전체 삭제 하시겠습니까?")) {
+        localStorage.clear();
+      }
+    },
+  },
+};
+</script>
+
+<style>
+</style>
