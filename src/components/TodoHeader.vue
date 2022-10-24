b2e4fe74861040a107da9d1090c570ba12c3b14d
summer <summery.dev@gmail.com>
2022-10-24 15:02:37 +0900

    todoHeader 작성

---
+++ src/components/TodoHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <header><h1>my TODOs</h1></header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+h1 {
+  color: rgb(67, 67, 67);
+  font-weight: 900;
+  margin: 2.5rem 0 1.5rem;
+}
+</style>
