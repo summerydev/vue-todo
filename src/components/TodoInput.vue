c2cae2e79b8f0bd6142b3f8ebd45a345c38282f2
summer <summery.dev@gmail.com>
2022-10-24 15:03:52 +0900

    - todo입력 후  로컬 스토리지에 저장(버튼 클릭, enter 시) - 저장 후 input clear

---
+++ src/components/TodoInput.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodo" />
+    <button v-on:click="addTodo">+</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => {
+    return {
+      newTodoItem: "",
+    };
+  },
+  methods: {
+    addTodo() {
+      console.log(this.newTodoItem);
+      localStorage.setItem(this.newTodoItem, this.newTodoItem);
+      this.clearInput();
+    },
+    clearInput() {
+      this.newTodoItem = "";
+    },
+  },
+};
+</script>
+
+<style>
+</style>
