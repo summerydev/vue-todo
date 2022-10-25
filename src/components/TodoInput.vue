a310d9122662bcc4153f9e426168d8818d352b48
summer <summery.dev@gmail.com>
2022-10-25 14:28:07 +0900

    미입력 시 모달 기능 추가

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -2,28 +2,39 @@
   <div>
     <input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodo" />
     <button v-on:click="addTodo">+</button>
+    <Modal v-if="showModal" @close="showModal = false">
+      <h3 slot="header">🧐 <button @click="showModal = false">❌</button></h3>
+      <div slot="body">아무것도 입력하지 않으셨네요!</div>
+    </Modal>
   </div>
 </template>
 
 <script>
+import Modal from "./common/Modal.vue";
+
 export default {
   data: () => {
     return {
       newTodoItem: "",
+      showModal: false,
     };
   },
+
   methods: {
     addTodo() {
       if (this.newTodoItem !== "") {
-        this.$emit("addTodoItem", this.newTodoItem)
+        this.$emit("addTodoItem", this.newTodoItem);
         this.clearInput();
+      } else {
+        this.showModal = !this.showModal;
       }
     },
-
     clearInput() {
       this.newTodoItem = "";
     },
   },
+
+  components: { Modal: Modal },
 };
 </script>
 
