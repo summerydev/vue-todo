2d52277b8445ebe8eaf8f7634456b40210735331
summer <summery.dev@gmail.com>
2022-10-25 14:27:54 +0900

    전체 삭제 모달 기능 추가

--- src/components/TodoFooter.vue
+++ src/components/TodoFooter.vue
@@ -1,17 +1,37 @@
 <template>
   <footer>
-    <button v-on:click="clearAll">전체 삭제</button>
+    <button @click="showModal = true">전체 삭제</button>
+    <Modal v-if="showModal" @close="showModal = false">
+      <h1 slot="header">⚠️</h1>
+      <div slot="body">
+        모두 삭제하시겠습니까?
+        <div>
+          <button @click="showModal = false">취소</button>
+          <button v-on:click="clearAll" @click="showModal = false">
+            삭제하기
+          </button>
+        </div>
+      </div>
+    </Modal>
   </footer>
 </template>
 
 <script>
+import Modal from "./common/Modal.vue";
+
 export default {
+  data: () => {
+    return {
+      showModal: false,
+    };
+  },
   methods: {
     clearAll() {
-      if (confirm("전체 삭제 하시겠습니까?")) {
-        this.$emit("clear");
-      }
+      this.$emit("clear");
     },
+  },
+  components: {
+    Modal: Modal,
   },
 };
 </script>
