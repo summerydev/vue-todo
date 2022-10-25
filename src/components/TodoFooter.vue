0e62d1533d4ecb2a9fe66cab6e8e34a037ec9243
summer <summery.dev@gmail.com>
2022-10-25 15:23:26 +0900

    삭제 항목 없을 시 확인 모달 기능 추가

--- src/components/TodoFooter.vue
+++ src/components/TodoFooter.vue
@@ -1,7 +1,14 @@
 <template>
   <footer>
-    <button @click="showModal = true">전체 삭제</button>
-    <Modal v-if="showModal" @close="showModal = false">
+    <button
+      @click="
+        showModal = true;
+        checkData;
+      "
+    >
+      전체 삭제
+    </button>
+    <Modal v-if="showModal && propsdata.length != 0" @close="showModal = false">
       <h1 slot="header">⚠️</h1>
       <div slot="body">
         모두 삭제하시겠습니까?
@@ -13,6 +20,16 @@
         </div>
       </div>
     </Modal>
+    <Modal
+      v-else-if="showModal && propsdata.length == 0"
+      @close="showModal = false"
+    >
+      <h1 slot="header">🤓</h1>
+      <div slot="body">
+        삭제할 항목이 없습니다.
+        <button @click="showModal = false">닫기</button>
+      </div>
+    </Modal>
   </footer>
 </template>
 
@@ -20,6 +37,8 @@
 import Modal from "./common/Modal.vue";
 
 export default {
+  props: ["propsdata"],
+
   data: () => {
     return {
       showModal: false,
@@ -29,7 +48,12 @@ export default {
     clearAll() {
       this.$emit("clear");
     },
+
+    checkData() {
+      this.propsdata.length == 0 ? false : true;
+    },
   },
+
   components: {
     Modal: Modal,
   },
