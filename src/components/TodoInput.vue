7eb05f9aa9323a3bc4f78fc5bb7d18a9fc940bb4
summer <summery.dev@gmail.com>
2022-10-26 13:57:12 +0900

    리팩토링 : todo 추가 시 store 로직으로 변경

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -28,18 +28,19 @@ export default {
   methods: {
     addTodo() {
       if (this.newTodoItem !== "") {
-        this.$emit("addTodoItem", this.newTodoItem);
+        this.$store.commit("addOneItem", this.newTodoItem);
         this.clearInput();
       } else {
         this.showModal = !this.showModal;
       }
     },
+    
     clearInput() {
       this.newTodoItem = "";
     },
   },
 
-  components: { Modal},
+  components: { Modal },
 };
 </script>
 
