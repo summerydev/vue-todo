220f3a661826185e79721833f4f51139c6e07cc9
summer <summery.dev@gmail.com>
2022-10-26 17:04:14 +0900

    리팩토링 : mutations 모듈화

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import * as mutations from "./mutations";
 
 Vue.use(Vuex);
 
@@ -20,40 +21,12 @@ export const store = new Vuex.Store({
     headerText: "My Todos",
     todoItems: storage.fetch(),
   },
-  
+
   getters: {
     storedTodoItems(state) {
       return state.todoItems;
     },
   },
 
-  mutations: {
-    addOneItem(state, todoItem) {
-      const obj = { completed: false, item: todoItem };
-      localStorage.setItem(todoItem, JSON.stringify(obj));
-      state.todoItems.push(obj);
-    },
-
-    removeOneItem(state, payload) {
-      localStorage.removeItem(payload.todoItem.item);
-      state.todoItems.splice(payload.index, 1);
-    },
-
-    completeOneItem(state, payload) {
-      state.todoItems[payload.index].completed =
-        !state.todoItems[payload.index].completed;
-
-      localStorage.removeItem(payload.todoItem.item);
-
-      localStorage.setItem(
-        payload.todoItem.item,
-        JSON.stringify(payload.todoItem)
-      );
-    },
-
-    clearAllItems(state) {
-      localStorage.clear();
-      state.todoItems = [];
-    },
-  },
+  mutations,
 });
