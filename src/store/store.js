a9bb7920e549a3ee9906a7ed8221c59de6c1af91
summer <summery.dev@gmail.com>
2022-10-27 09:18:36 +0900

    리팩토링 : store 모듈화

--- src/store/store.js
+++ src/store/store.js
@@ -1,32 +1,9 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import * as mutations from "./mutations";
+import todoApp from "./modules/todoApp";
 
 Vue.use(Vuex);
 
-const storage = {
-  fetch() {
-    const arr = [];
-    if (localStorage.length > 0) {
-      for (let i = 0; i < localStorage.length; i++) {
-        arr.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
-      }
-    }
-    return arr;
-  },
-};
-
 export const store = new Vuex.Store({
-  state: {
-    headerText: "My Todos",
-    todoItems: storage.fetch(),
-  },
-
-  getters: {
-    storedTodoItems(state) {
-      return state.todoItems;
-    },
-  },
-
-  mutations,
+  modules: { todoApp },
 });
